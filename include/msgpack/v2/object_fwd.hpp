1c32145e3586b32afe3dda8f3510a8d4ad50be45
akashin <kashin.andrej@gmail.com>
2017-05-12 10:01:52 +0300

    Rename `check` to avoid clash with UE4 define.

--- include/msgpack/v2/object_fwd.hpp
+++ include/msgpack/v2/object_fwd.hpp
@@ -79,22 +79,22 @@ template <typename T>
 struct has_as {
 private:
     template <typename U>
-    static auto check(U*) ->
+    static auto check_(U*) ->
         typename std::enable_if<
-            // check v2 specialization
+            // check_ v2 specialization
             std::is_same<
                 decltype(adaptor::as<U>()(std::declval<msgpack::object>())),
                 U
             >::value
             ||
-            // check v1 specialization
+            // check_ v1 specialization
             v1::has_as<U>::value,
             std::true_type
         >::type;
     template <typename>
-    static std::false_type check(...);
+    static std::false_type check_(...);
 public:
-    using type = decltype(check<T>(MSGPACK_NULLPTR));
+    using type = decltype(check_<T>(MSGPACK_NULLPTR));
     static constexpr bool value = type::value;
 };
 
